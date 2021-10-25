04e283b1d07217c8de63e1781eccc4a752c429e4
othienoJoe <bryo094@gmail.com>
2021-10-25 14:33:10 +0300

    created ana serializer folder and installed psycopg2

--- rator/urls.py
+++ rator/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('projects.urls')),
-    path('', include('users.urls')),
+    path('', include('app.urls')),
 ]
