c9599788edb4762a5e119087b9ca4c8e1a940073
othienoJoe <bryo094@gmail.com>
2021-10-25 10:44:59 +0300

    updated the models file to create and save the User

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 from cloudinary.models import CloudinaryField
 
 # Create your models here.
@@ -8,3 +10,14 @@ class Profile(models.Model):
 	description = models.TextField()
 	user = models.OneToOneField(User,on_delete=models.CASCADE)
 	email = models.EmailField()
+
+	def __str__(self):
+		return self.user.username
+	@receiver(post_save, sender = User)
+	def create_profile(sender, instance, created, **kwargs):
+		if created:
+			Profile.objects.create(user = instance)
+
+	@receiver(post_save, sender = User)
+	def save_profile(sender, instance, **kwargs):
+		instance.profile.save()
