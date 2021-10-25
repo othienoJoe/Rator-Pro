64cb636288298d79402147afd5e50cf28fd1eec3
othienoJoe <bryo094@gmail.com>
2021-10-25 12:59:31 +0300

    restructured the logout.html file and aligned its path

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
-from users.views import register_user
+from .views import register_user
 
 urlpatterns = [
 	path('register/', register_user, name='register'),
 	path('profile/', views.profile_view, name='profile'),
+	path('login/', LoginView.as_view(template_name = 'users/login.html'), name = 'login'),
+	path('logout/', LogoutView.as_view(template_name = 'users/logout.html'), name = 'login'),
 ]
