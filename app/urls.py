c1f374c4f1dc7aba2b329dbe2563538425148a0f
othienoJoe <bryo094@gmail.com>
2021-10-25 14:52:11 +0300

    updated the urls and signals files

--- app/urls.py
+++ app/urls.py
@@ -2,10 +2,16 @@ from django.urls import path
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import register_user
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 	path('register/', register_user, name='register'),
 	path('profile/', views.profile_view, name='profile'),
 	path('login/', LoginView.as_view(template_name = 'users/login.html'), name = 'login'),
-	path('logout/', LogoutView.as_view(template_name = 'users/logout.html'), name = 'login'),
+	path('logout/', LogoutView.as_view(template_name = 'users/logout.html'), name = 'logout'),
+	path('update/', views.update, name='update'),
 ]
+
+if settings.DEBUG:
+	urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
